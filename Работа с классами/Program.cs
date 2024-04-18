790bc419cb923e73a4d3e9fb4239a5fb15020fef
leatherbullet <al.obnizov@yandex.ru>
2024-04-18 11:09:58 +0300

    Добавьте файлы проекта.

---
+++ Работа с классами/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Работа_с_классами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Василий", 100, 5);
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _level;
+
+        public Player(string name, int health, int level)
+        {
+            _name = name;
+            _health = health;
+            _level = level;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"имя {_name} здоровье {_health} уровень {_level}");
+        }
+    }
+}
